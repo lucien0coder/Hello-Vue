ad001d3e86a1aee1a725b8a4888e111ec0a3dc7e
lucien_zhu <lucien_zhu@qq.com>
2016-12-23 18:30:32 +0800

    更新选人

--- choice/choice.js
+++ choice/choice.js
@@ -8,7 +8,34 @@ Vue.directive('focus', {
 })
 
 //组件
+//选项卡组件
+Vue.component('area-tap',{
+	props:['title'],
+	template:'#tab-template',
+	data:function(){
+		return {
+			tab_seen:true,
+			isActive:false
+		}
+	},
+	methods:{
+		getArea:function(e){
+			alert(this.title)
+			console.log(this.isActive)
+			if(this.isActive) this.isActive = false
+			else this.isActive = true
+		},
+	}
+})
 
+//角色、人员列表组件
+Vue.component('select-item',{
+	template:'#item-template'
+})
+//选中人员列表组件
+Vue.component('pitckup-staff',{
+	template:'#staff-template'
+})
 
 //实例
 let cv = new Vue({
@@ -19,7 +46,73 @@ let cv = new Vue({
 		seen_org:true,
 		seen_dept:true,
 		seen_band:true,
-		searchValue:''
+		searchValue:'',
+		isActive:false,
+		show:true,
+		pitck_ons:[
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+				{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'优乐美'},
+			{name:'麦克吉'},
+			{name:'小宝度'},
+			{name:'王欧密'},
+		],
+		roles:[
+			{roleName:'开发员'},
+			{roleName:'开发组长'},
+			{roleName:'开发主管'},
+			{roleName:'项目经理'}
+		],
+		staffs:[
+			{name:'李大福'},
+			{name:'张优美'},
+			{name:'黄向丽'},
+			{name:'开往来'},
+			{name:'李由'},
+			{name:'尚小龙'},
+			{name:'曲立东'}
+		],
+		roleTitle:'角色列表',
+		titles:['区人员','部门人员','机构人员','客户','平台']
 	},
 	created:function(){
 		console.log('create: hello choice_app')
@@ -27,24 +120,32 @@ let cv = new Vue({
 	methods:{
 		getPart:function(e){
 			console.log('gp')
+
 		},
 		getCus:function(e){
 			console.log('gc')
 		},
 		getOrg:function(e){
 			console.log('go')
+			alert(e)
 		},
 		getBand:function(e){
 			console.log('gb')
 		},
-		getDept:function(e){
-			console.log('gd')
-			alert(e)
-		},
+		
 		search_some:function(e){
 			alert(this.searchValue)
+		},
+		confirm:function(){
+			alert(this.pitck_on)
+		},
+		clickStaff:function(e){
+			console.log(this.html)
+			this.show = false;
+			let targetValue = e.target.value;
+			console.log(targetValue)
 		}
 	}
 })
 
-cv.seen_org = false;
+// cv.seen_org = false;
