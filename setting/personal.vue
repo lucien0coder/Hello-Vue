55749fbe5e74106fc02ca3eb7ad587ed94b24b5b
lucienZhu <lucien_zhu@qq.com>
2017-01-25 10:09:48 +0800

    试用vue-router

--- setting/personal.vue
+++ setting/personal.vue
@@ -1,5 +1,119 @@
 <template>
-	
+
+	<div class="tile is-ancestor">
+	  <div class="tile is-4 is-vertical is-parent">
+	    <div class="tile is-child" style="border-bottom: 1px solid #ece8e8;">
+          <figure class="image is-3by2">
+            <img src="img.jpg" style="border-radius: 0.4em;width: 70%;left:10%">
+          </figure>
+	      <div class="hero-body" style="padding: 1rem;">
+	        <div class="container">
+	          <h1 class="title" style="margin-bottom: 0.5rem;">
+	            张海涛
+	          </h1>
+	          <h1 class="authentication heig_class_atc">高级认证</h1>
+	          <h2 class="subtitle">
+	            @zhanghaitao
+	          </h2>
+	          <blockquotes class="subtitle is-6" style="color: darkgray;">
+	        	一个勤勤恳恳种代码的农夫
+	          </blockquotes>
+	        </div>
+	      </div>
+	    </div>
+	    <div class="tile is-child">
+	      <div class="content">
+            <span class="icon">
+              <i class="fa fa-clock-o"></i>	
+            </span>
+            <span>11:09 PM - 1 Jan 2016</span>
+          </div>
+	      <div class="content">
+	    	<span class="icon">
+              <i class="fa fa-map-marker"></i>	
+            </span>
+            <span>China, GuangDong</span>
+	      </div>
+		  <div class="content">
+		  	<span class="icon">
+		  	  <i class="fa fa-building"></i>
+		  	</span>
+		  	<span>South China University of Technology</span>
+		  </div>
+		  <div class="content" style="border-top: 1px solid #ece8e8;">
+		  	<div class="edit_btn">
+	  		  <a class="button">
+		        <span class="icon"><i class="fa fa-pencil-square-o"></i></span>
+		        <span>修改基础信息</span>
+		  	  </a>
+		  	</div>
+		  	<div class="edit_btn">
+			  <a class="button">
+			    <span class="icon"><i class="fa fa-unlock-alt"></i></span>
+			    <span>重置安全信息</span>
+			  </a>
+		  	</div>
+		  	<div class="edit_btn">
+			  <a class="button">
+			    <span class="icon"><i class="fa fa-check-square-o"></i></span>
+			    <span>增加认证信息</span>
+			  </a>
+		  	</div>
+		  </div>
+	    </div>
+	  </div>
+	  <div class="tile is-parent">
+	    <div class="tile is-child content">
+		  <h4><em>账号默认配置</em></h4>
+		  <hr>
+		  <div class="container">
+			<div class="notification is-warning">
+			   <small> 介绍下什么是 <strong> 「默认单位」</strong> ？</small>
+			</div>
+		  	<div class="control is-horizontal">
+			  <div class="control-label">
+			    <label class="label">当前默认单位</label>
+			  </div>
+			  <div class="control">
+			    <div class="select is-fullwidth">
+			      <select>
+			        <option>华南理工大学</option>
+			      </select>
+			    </div>
+			  </div>
+			</div>
+			<div class="notification is-warning">
+			    <small>说说什么是 <strong> 「默认打开帮区」</strong> ？</small>
+			</div>
+			<div class="control is-horizontal">
+			  <div class="control-label">
+			    <label class="label">默认打开帮区</label>
+			  </div>
+			  <div class="control">
+			    <div class="select is-fullwidth">
+			      <select>
+			        <option>B8实验室</option>
+			      </select>
+			    </div>
+			  </div>
+			</div>
+			<div class="content">
+				<hr>
+				<h4><em>账号认证信息</em></h4>
+				<hr>
+				<ul>
+			      <li><label>用户账号：</label>zhanghaitao</li>
+			      <li><label>手机号码：</label>1367*****93</li>
+			      <li><label>电子邮箱：</label>luc******@qq.com</li>
+			      <li><label>身份证号：</label>44*************530</li>
+			      <li><label>证件快照：</label><a href="">点击查看</a></li>
+			    </ul>
+			</div>
+
+		  </div>
+	    </div>
+	  </div>
+	</div>
 
 
 </template>
--- setting/setting.js
+++ setting/setting.js
@@ -1,1 +1,15 @@
 var personal_info = {template: 'personal.vue'}
+var personal_setting = {template: 'personal_setting.vue'}
+
+var routes = [
+	{path:'/info', component: personal_info},
+	{path:'/setting', component:personal_setting}
+]
+
+var router = new VueRouter({
+	routes :routes
+})
+
+var vm = new Vue({
+	router: router
+}).$mount('#personal_setting')
