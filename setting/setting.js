d99081860d8855680954995b04375ae4373e5d18
lucien_zhu <lucien_zhu@qq.com>
2017-02-04 18:13:19 +0800

    profile 部分的交互

--- setting/setting.js
+++ setting/setting.js
@@ -1,18 +1,43 @@
-// import personalInfo from 'personal'
-// import personalSetting from 'personalSetting'
+var persons = Vue.extend({
+			template:'#ps',
+			data: function(){
+				return {
+					msg:'hello vue router component'
+				}
+			}
+		})
 
-var personalInfo = {template: '<div>MYInfo</div>'}
-var personalSetting = {template: 'personalSetting.vue'}
+		var personi = Vue.extend({
+			template:'#pi',
+			data: function(){
+				return{
+					userName:'张海涛',
+					useraccount:'zhanghaitao',
+					bio:'一个勤勤恳恳种代码的农夫',
+					registerDate:'11:09 PM - 1 Jan 2016',
+					local:'China, GuangDong',
+					organization:'South China University of Technology',
+					phoneNO:'1367*****93',
+					eMail:'luc******@qq.com',
+					ID:'44*************530',
+					level:'高级认证'
+				}
+			},
+			methods:{
+				
+			}
+		})
 
-var routes = [
-	{path:'/info', component: personalInfo},
-	{path:'/setting', component:personalSetting}
-]
+		var routes = [
+			{path:'/', component: personi},
+			{path:'/setting', component: persons}
+		]
 
-var router = new VueRouter({
-	routes :routes
-})
+		var router = new VueRouter({
+			routes :routes
+		})
 
-var vm = new Vue({
-	router: router
-}).$mount('#personal_setting')
+		var rou = new Vue({
+			router: router,
+			
+		}).$mount('#app')
